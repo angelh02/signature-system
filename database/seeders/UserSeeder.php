6f310288c6a8b335a72965a65112d968a8b4e03c
angelh02 <angelhurtadom02@gmail.com>
2022-09-28 16:59:15 -0500

    task: adding properties to user model to use roles from spatie library, also adding functions and api routes to create a CRUD for users, and updating user seeder to create two user and two roles for admin and signer

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,12 +15,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //User
-        User::factory([
+        $adminRole = Role::create([
+            'name' => 'Administrador'
+        ]);
+        //Admin user
+        $admin = User::factory([
             "name" => "Administrador",
             "surnames" => "Root",
             "user_name" => "admin",
             "email" => "admin@admin.com"
         ])->create();
+        $admin->assignRole($adminRole);
+
+        $signerRole = Role::create([
+            'name' => 'Firmante'
+        ]);
+        //Signer user
+        $signer = User::factory([
+            'name' => 'Firmante',
+            'surnames' => 'Usuario',
+            'user_name' => 'signer',
+            'email' => 'signer@signer.com'
+        ])->create();
+        $signer->assignRole($signerRole);
     }
 }
